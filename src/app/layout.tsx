7048a8d72d5bb808fc832a533e655d5340ad9585
jasonwilkens <jasonwilkens@gmail.com>
2025-06-14 13:18:45 -0700

    iosevka

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const iosevka = localFont({
+  src: "../../public/fonts/IosevkaCustom-Regular.woff2",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={iosevka.className}>{children}</body>
     </html>
   );
 }
