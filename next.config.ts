e07a7e0447ac090fd630a5ad85bbac955f4c60ea
jasonwilkens <jasonwilkens@gmail.com>
2025-10-04 07:04:47 -0700

    config redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/harrow",
+        destination: "https://old.jasonwilkens.website/harrow",
+        permanent: true,
+      },
+      {
+        source: "/pathfinder/:slug",
+        destination: "https://old.jasonwilkens.website/pathfinder/:slug",
+        permanent: true,
+      },
+      {
+        source: "/pathfinder",
+        destination: "https://old.jasonwilkens.website/pathfinder",
+        permanent: true,
+      },
+      {
+        source: "/modern-pathfinder",
+        destination: "https://old.jasonwilkens.website/modern-pathfinder",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/harrow/page.tsx
+++
@@ -1,5 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-export default function HarrowRedirect() {
-  permanentRedirect("https://old.jasonwilkens.website/harrow");
-}
